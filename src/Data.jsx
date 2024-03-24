4b37bc11d6c277c7bfb2255de4d97dc3499db168
mdnoorulain <mdnoorulain786.mna@gmail.com>
2024-03-24 14:33:15 +0530

    create page for founder

--- src/Data.jsx
+++ src/Data.jsx
@@ -12,12 +12,7 @@ import courses6 from "./assets/courses6.jpg";
 import courses7 from "./assets/courses7.jpg";
 import courses8 from "./assets/courses8.jpg";
 
-import logo1 from "./assets/logo1.png";
-import logo2 from "./assets/logo2.png";
-import logo3 from "./assets/logo3.png";
-import logo4 from "./assets/logo4.png";
-import logo5 from "./assets/logo5.png";
-import logo6 from "./assets/logo6.png";
+
 import mtzlogo from "./assets/mtzlogo.png";
 
 export const logo = [mtzlogo];
@@ -114,8 +109,6 @@ export const courses = [
     title: "MTZIC festiv",
   },
 ];
-
-export const logos = [logo1, logo2, logo3, logo4, logo5, logo6];
 
 export const accordions = [
   {
--- src/components/details/founder.jsx
+++ src/components/details/founder.jsx
@@ -1,89 +1,33 @@
 import React from "react";
-import { techdetails } from "../../Data";
-import { Link } from 'react-router-dom'
-const userProfile = techdetails;
-
-
-
+import founderimg from "../../assets/founder.png";
+import {Navbar} from '../../components/index'
 function founder() {
   return (
-    <div className="font-Poppins bg-Solitude">
-      <div className="profile-bg-image w-full h-44 sm:h-64 md:h-80"></div>
-      <div className="profile-description flex flex-col justify-start items-start md:justify-center md:items-center gap-5 sm:gap-8 md:gap-10 px-2 sm:px-4 md:px-14">
-        <div className="photo-intro gap-3 flex">
-          <div className="profile-photo border-2 -my-3 sm:-my-5 md:-my-12 border-gray-500 rounded-full w-16 h-16 sm:w-24 sm:h-24 md:w-44 md:h-44"></div>
-          <div className="profile-name py-2 mx-0 sm:mx-1 md:mx-2 flex flex-col gap-3 justify-start items-between">
-            <span className="name flex gap-2 justify-center items-center w-fit font-bold text-xl sm:text-2xl md:text-3xl">
-              {userProfile.name}
-              <i className="fa-regular fa-gem flex justify-center items-center p-1 text-xs sm:text-sm bg-purple-900 text-white w-5 h-5 rounded-full"></i>
-              <i className="fa-solid fa-check flex justify-center items-center p-1 text-xs sm:text-sm bg-green-400 text-black font-bold w-5 h-5 rounded-full"></i>
-            </span>
-            <div className="follow-details flex gap-4 font-bold text-base sm:text-md md:text-lg">
-              <div className="followers-details flex flex-col">
-                <span className="followers rounded-lg text-center px-2 sm:px-4 sm:py-1 border-2 text-gray-400 border-gray-400 hover:bg-gray-400 hover:text-white duration-300 cursor-pointer">
-                  {userProfile.followers}
-                </span>
-                <span className="followers-text text-center text-gray-400 text-sm sm:text-base font-medium">
-                  Followers
-                </span>
-              </div>
-              <div className="following-details flex flex-col">
-                <span className="following rounded-lg text-center px-2 sm:px-4 sm:py-1 border-2 text-gray-400 border-gray-400 hover:bg-gray-400 hover:text-white duration-300 cursor-pointer">
-                  {userProfile.following}
-                </span>
-                <span className="following-text text-center text-gray-400 text-sm sm:text-base font-medium">
-                  Following
-                </span>
-              </div>
-            </div>
-          </div>
+    <div className=" mx-auto px-4 py-8  bg-slate-100 w-screen h-screen">
+      <div><Navbar /></div>
+      <div className="grid grid-cols-1 md:grid-cols-2 mt-10 ">
+        <div className="flex flex-col items-center justify-center w-4/5">
+          <img
+            src={founderimg}
+            alt="Founder profile"
+            className="w-48 h-48 rounded-full object-cover "
+          />
+          <h2 className="text-2xl font-bold mt-4">M.T.Z Ashrafi</h2>
         </div>
-        <div className="photo-details flex flex-col gap-2 w-fit">
-          <h3 className="profession text-md md:text-lg">
-            {userProfile.profession}
-          </h3>
-          <a
-            href={userProfile.instagram}
-            className="instagram-link text-blue-500 font-semibold text-sm md:text-lg hover:underline"
-            target="_blank"
-            rel="noreferrer"
-          >
-            {userProfile.instagram}
-          </a>
-          <div className="reactions flex flex-col sm:flex-row flex-wrap p-1 gap-8 text-xs font-semibold">
-            <span className="flex gap-2">
-              <i className="fa-regular fa-star flex justify-center items-center p-1 bg-cyan-600 text-white text-sm w-6 h-6 rounded-full"></i>
-              <span className="py-0.5 text-sm font-semibold">
-                {userProfile.favorites}
-              </span>
-            </span>
-            <span className="flex gap-2">
-              <i className="fa-regular fa-thumbs-up flex justify-center items-center p-1 bg-yellow-600 text-white text-sm w-6 h-6 rounded-full"></i>
-              <span className="py-0.5 text-sm font-semibold">
-                {userProfile.thumbLikes}
-              </span>
-            </span>
-            <span className="flex gap-2">
-              <i className="fa-regular fa-eye flex justify-center items-center p-1 bg-gray-600 text-white text-sm w-6 h-6 rounded-full"></i>
-              <span className="py-0.5 text-sm font-semibold">
-                {userProfile.views}
-              </span>
-            </span>
-            <span className="flex gap-2">
-              <i className="fa-regular fa-heart flex justify-center items-center p-1 bg-red-600 text-white text-sm w-6 h-6 rounded-full"></i>
-              <span className="py-0.5 text-sm font-semibold">
-                {userProfile.heartLikes}
-              </span>
-            </span>
-          </div>
+        <div className=" mt-10 m-7 ">
+          <h3 className="text-xl font-semibold">About Me</h3>
+          <p className="text-gray-700 mt-4">
+            In the heart of every dream lies the seed of possibility, waiting to
+            be nurtured by passion and perseverance. With unwavering dedication,
+            M.T.Z Ashrafi planted the seeds of MTZIC Education, cultivating a
+            garden of knowledge where aspirations blossom into reality. His
+            vision illuminates the path to knowledge, fostering a community
+            where dreams take flight. With unwavering dedication, he shapes
+            futures and ignites passions. Through his leadership, MTZIC
+            Education transcends boundaries, empowering minds to soar to new
+            heights.
+          </p>
         </div>
-
-        <Link
-          to="/"
-          className="py-3 px-4 bg-Teal text-white rounded-lg text-sm font-bold "
-        >
-          Back
-        </Link>
       </div>
     </div>
   );
